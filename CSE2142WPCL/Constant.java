3841afd13d00325b035188ff453770a6a7c3f6f3
Sanjoy Kumar <sanjoy.ru.cse@gmail.com>
2022-06-21 20:34:04 +0600

    Step 4 and Step 5 are completed.

---
+++ CSE2142WPCL/Constant.java
@@ -0,0 +1,19 @@
+public class Constant {
+    public static String inputFileName = "students.txt";
+    public static String errorMessage = "Usage:  a|r|c|+word|?word";
+    public static String displayCommand = "a";
+    public static String addCommand = "+";
+    public static String searchCommand = "?";
+    public static String countCommand = "c";
+    public static String randomAccessCommand = "r";
+    public static String waitingMessage = "Loading data ...";
+    public static String endMessage = "Data Loaded.";
+    public static String separator = ",";
+    public static String timeFormat = "dd/mm/yyyy-hh:mm:ss a";
+    public static String singleSpace = " ";
+    public static char singleSpaceChar = ' ';
+    public static String updateMessage = "\nList last updated on ";
+    public static String foundMessage = "We found it!";
+    public static String countMessage = " word(s) found ";
+
+}
--- CSE2142WPCL/StudentList.java
+++ CSE2142WPCL/StudentList.java
@@ -7,102 +7,92 @@ public class StudentList {
 
         if(args.length!=1)
         {
-            System.out.println("Sorry Sir! Your Argument is wrong.\nPlease give the right argument and run the program again.");
-            System.out.println("\na :for print contains.");
-            System.out.println("?word :for searching the word");
-            System.out.println("+word :for add words.");
-            System.out.println("c :for counting space");
+            System.out.println(Constant.errorMessage);
             return;
         }
 //		Check arguments
-        if (args[0].equals("a")) {
-            System.out.println("Loading data ...");
+        if (args[0].equals(Constant.displayCommand)) {
+            System.out.println(Constant.waitingMessage);
             try {
                 String line = getString();
-                String names[] = line.split(",");
+                String names[] = line.split(Constant.separator);
                 for (String name : names) {
                     System.out.println(name.trim());
                 }
             } catch (Exception e) {
             }
-            System.out.println("Data Loaded.");
-        } else if (args[0].equals("r")) {
-            System.out.println("Loading data ...");
+            System.out.println(Constant.endMessage);
+        } else if (args[0].equals(Constant.randomAccessCommand)) {
+            System.out.println(Constant.waitingMessage);
             try {
                 String line = getString();
-                String names[] = line.split(",");
-
+                String names[] = line.split(Constant.separator);
                 int randomIndex = (int) ((Math.random() * ((names.length-1) - 0)));
                 System.out.println(names[randomIndex].trim());
             } catch (Exception e) {
             }
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("+")) {
-            System.out.println("Loading data ...");
+            System.out.println(Constant.endMessage);
+        } else if (args[0].contains(Constant.addCommand)) {
+            System.out.println(Constant.waitingMessage);
             try {
                 BufferedWriter bufferedReader = getBufferedWriter();
                 String subStringToAdd = args[0].substring(1);
                 Date date = new Date();
-                String timeFormat = "dd/mm/yyyy-hh:mm:ss a";
+                String timeFormat = Constant.timeFormat;
                 DateFormat dateFormat = new SimpleDateFormat(timeFormat);
                 String formatedDate = dateFormat.format(date);
-                bufferedReader.write(", " + subStringToAdd + "\nList last updated on " + formatedDate);
+                bufferedReader.write(Constant.separator + Constant.singleSpace + subStringToAdd + Constant.updateMessage + formatedDate);
                 bufferedReader.close();
             } catch (Exception e) {
             }
 
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("?")) {
-            System.out.println("Loading data ...");
+            System.out.println(Constant.endMessage);
+        } else if (args[0].contains(Constant.searchCommand)) {
+            System.out.println(Constant.waitingMessage);
             try {
                 String line = getString();
-                String names[] = line.split(",");
+                String names[] = line.split(Constant.separator);
                 boolean done = false;
                 String subStringToAdd = args[0].substring(1);
                 for (int id = 0; id < names.length && !done; id++) {
                     if (names[id].trim().equals(subStringToAdd)) {
-                        System.out.println("We found it!");
+                        System.out.println(Constant.foundMessage);
                         done = true;
                     }
                 }
             } catch (Exception e) {
             }
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("c")) {
-            System.out.println("Loading data ...");
+            System.out.println(Constant.endMessage);
+        } else if (args[0].contains(Constant.countCommand)) {
+            System.out.println(Constant.waitingMessage);
             try {
                 String line = getString();
                 char alphabates[] = line.toCharArray();
                 int count = 0;
                 for (char space : alphabates) {
-                    if (space == ' ') {
+                    if (space == Constant.singleSpaceChar) {
                         count++;
                     }
                 }
-                System.out.println(count+1 + " word(s) found ");
+                System.out.println(count+1 + Constant.countMessage);
             } catch (Exception e) {
             }
-            System.out.println("Data Loaded.");
+            System.out.println(Constant.endMessage);
         } else {
-            System.out.println("Sorry Sir! Your Argument is wrong.\nPlease give the right argument and run the program again.");
-            System.out.println("\na :for print contains.");
-            System.out.println("?word :for searching the word");
-            System.out.println("+word :for add words.");
-            System.out.println("c :for counting space");
-
+            System.out.println(Constant.errorMessage);
         }
     }
 
     private static BufferedWriter getBufferedWriter() throws IOException {
         BufferedWriter bufferedReader = new BufferedWriter(
-                new FileWriter("students.txt", true));
+                new FileWriter(Constant.inputFileName, true));
         return bufferedReader;
     }
 
     private static String getString() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(
                 new InputStreamReader(
-                        new FileInputStream("students.txt")));
+                        new FileInputStream(Constant.inputFileName)));
         return bufferedReader.readLine();
     }
 }
