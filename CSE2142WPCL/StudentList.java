16911f05f4f8f06a3fa0eb0e1683b35d982b491a
Sanjoy Kumar <sanjoy.ru.cse@gmail.com>
2022-06-21 12:55:13 +0600

    Step -3: Refactor has been completed

--- CSE2142WPCL/StudentList.java
+++ CSE2142WPCL/StudentList.java
@@ -5,14 +5,20 @@ import java.util.*;
 public class StudentList {
     public static void main(String[] args) {
 
+        if(args.length!=1)
+        {
+            System.out.println("Sorry Sir! Your Argument is wrong.\nPlease give the right argument and run the program again.");
+            System.out.println("\na :for print contains.");
+            System.out.println("?word :for searching the word");
+            System.out.println("+word :for add words.");
+            System.out.println("c :for counting space");
+            return;
+        }
 //		Check arguments
         if (args[0].equals("a")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
-                String line = bufferedReader.readLine();
+                String line = getString();
                 String names[] = line.split(",");
                 for (String name : names) {
                     System.out.println(name.trim());
@@ -23,10 +29,7 @@ public class StudentList {
         } else if (args[0].equals("r")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
-                String line = bufferedReader.readLine();
+                String line = getString();
                 String names[] = line.split(",");
 
                 int randomIndex = (int) ((Math.random() * ((names.length-1) - 0)));
@@ -37,8 +40,7 @@ public class StudentList {
         } else if (args[0].contains("+")) {
             System.out.println("Loading data ...");
             try {
-                BufferedWriter bufferedReader = new BufferedWriter(
-                        new FileWriter("students.txt", true));
+                BufferedWriter bufferedReader = getBufferedWriter();
                 String subStringToAdd = args[0].substring(1);
                 Date date = new Date();
                 String timeFormat = "dd/mm/yyyy-hh:mm:ss a";
@@ -53,10 +55,7 @@ public class StudentList {
         } else if (args[0].contains("?")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
-                String line = bufferedReader.readLine();
+                String line = getString();
                 String names[] = line.split(",");
                 boolean done = false;
                 String subStringToAdd = args[0].substring(1);
@@ -72,10 +71,7 @@ public class StudentList {
         } else if (args[0].contains("c")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
-                String line = bufferedReader.readLine();
+                String line = getString();
                 char alphabates[] = line.toCharArray();
                 int count = 0;
                 for (char space : alphabates) {
@@ -95,5 +91,18 @@ public class StudentList {
             System.out.println("c :for counting space");
 
         }
+    }
+
+    private static BufferedWriter getBufferedWriter() throws IOException {
+        BufferedWriter bufferedReader = new BufferedWriter(
+                new FileWriter("students.txt", true));
+        return bufferedReader;
+    }
+
+    private static String getString() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream("students.txt")));
+        return bufferedReader.readLine();
     }
 }
